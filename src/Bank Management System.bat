9a2f45c90fc5d145b8abec5af5764ff300f5a453
Sazid <sazidozon@gmail.com>
2018-09-06 05:53:33 +0600

    Program runner

---
+++ src/Bank Management System.bat
@@ -0,0 +1,11 @@
+javac m6\*.java
+javac m6\components\*.java
+javac m6\table_model\*.java
+javac m6\ui\*.java
+
+java m6.Start
+
+del m6\*.class
+del m6\components\*.class
+del m6\table_model\*.class
+del m6\ui\*.class
